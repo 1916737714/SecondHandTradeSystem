341b3933a128773b5fa76014907bd7e3d9ca58d8
Proactive <2845350947@qq.com>
2024-07-31 21:05:28 +0800

    实现注册界面，完成界面跳转

--- app/src/main/java/com/example/andoirdsecondhandtradingsystem/Login.kt
+++ app/src/main/java/com/example/andoirdsecondhandtradingsystem/Login.kt
@@ -3,29 +3,17 @@ package com.example.andoirdsecondhandtradingsystem
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -36,96 +24,105 @@ import androidx.compose.ui.unit.sp
 
 class Login {
 
-@SuppressLint("NotConstructor")
-@Composable
-fun Login(message: String,from: String, modifier: Modifier = Modifier) {
-    var name by remember { mutableStateOf("") }
-    var pwd by remember { mutableStateOf("") }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("NotConstructor")
+    @Composable
+    fun Login(modifier: Modifier = Modifier) {
+        var name by remember { mutableStateOf("") }
+        var pwd by remember { mutableStateOf("") }
 
-    val pwdVisualTransformation = PasswordVisualTransformation()
-    var showPwd by remember { mutableStateOf(true) }
+        val pwdVisualTransformation = PasswordVisualTransformation()
+        var showPwd by remember { mutableStateOf(true) }
+        val transformation = if (showPwd) pwdVisualTransformation else VisualTransformation.None
 
-    val transformation = if (showPwd) pwdVisualTransformation else VisualTransformation.None
+        val rememberUser = remember { mutableStateOf(false) }
 
-    Box(modifier = modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.image5),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,//全屏填充
-            modifier = Modifier.fillMaxSize(),//填充全屏
-            alpha = 0.5f,//透明度
-        )
-        Column() {
-            Spacer(modifier = Modifier
-                .weight(3f)
-                .clip(RoundedCornerShape(16.dp)))
-            Column (
+        Box(modifier = modifier) {
+            Image(
+                painter = painterResource(id = R.drawable.image5),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
+                alpha = 0.5f,
+            )
+            Column(
                 modifier = Modifier
-                    .weight(3f)
-                    .background(color = Color.White)
+                    .fillMaxHeight(0.5f)
+                    .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)) // 增加圆角
+                    .background(Color.White) // 背景设置为白色
                     .padding(40.dp)
-                    .fillMaxSize()
-            ){
-                Column() {
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = name,
-                        placeholder = { Text("用户名") },
-                        onValueChange = {str -> name = str},
+                    .align(Alignment.BottomCenter)
 
-                        leadingIcon = {
-                            Icon(imageVector = Icons.Default.AccountBox,
-                                contentDescription = "用户名")
+
+            ) {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = name,
+                    placeholder = { Text("用户名") },
+                    onValueChange = { name = it },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.AccountBox, contentDescription = "用户名")
+                    },
+                    colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                TextField(
+                    value = pwd,
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = { Text("密码") },
+                    onValueChange = { pwd = it },
+                    visualTransformation = transformation,
+                    colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Lock, contentDescription = "密码")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = { showPwd = !showPwd }) {
+                            Icon(
+                                painter = painterResource(id = if (showPwd) R.drawable.eye_show else R.drawable.eye_hide),
+                                contentDescription = null,
+                                Modifier.size(24.dp)
+                            )
                         }
-
-
-                    )
-                    TextField(
-                        value = pwd,
-                        modifier = Modifier.fillMaxWidth(),
-                        placeholder = { Text("密码") },
-                        onValueChange = {str -> pwd = str},
-                        visualTransformation = transformation,
-
-                        leadingIcon = {
-                            Icon(imageVector = Icons.Default.Lock,
-                                contentDescription = "密码")
-                        },
-                        trailingIcon = {
-                            if (showPwd) {
-                                IconButton(onClick = { showPwd = !showPwd }) {
-                                    Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                                        contentDescription = null,
-                                        Modifier.size(24.dp)
-                                    )
-                                }
-                            } else {
-                                IconButton(onClick = { showPwd = !showPwd }) {
-                                    Icon(painter = painterResource(id = R.drawable.ic_launcher_background),
-                                        contentDescription = null,
-                                        Modifier.size(24.dp)
-                                    )
-                                }
-                            }
-                        },
-                        )
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-                Row(horizontalArrangement = Arrangement.SpaceBetween,modifier = Modifier.fillMaxWidth()) {
-                    Text(text = "记住密码", fontSize = 16.sp, color = Color.Gray)
-                    Text(text = "注册", fontSize = 16.sp, color = Color.Gray)
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Button(modifier = Modifier.fillMaxWidth(),
-                    onClick = { /*TODO*/ },
-                    shape = RoundedCornerShape(50),
-                    contentPadding = PaddingValues(12.dp,16.dp)
+                    },
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text("登录",color = Color.White,fontSize = 16.sp)
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = rememberUser.value,
+                            onCheckedChange = { rememberUser.value = it }
+                        )
+                        Text(text = "记住密码", color = Color.Gray)
+                    }
+                    Text(text = "注册", fontSize = 16.sp, color = Color.Gray, modifier = Modifier.padding(vertical = 12.dp, horizontal = 5.dp))
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // 创建渐变颜色的画刷
+                val gradientBrush = Brush.horizontalGradient(colors = listOf(Color(0xFFFFEB3B), Color(0xFFFF9800)))
+
+                // 使用 Box 来设置背景渐变
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50)) // 圆角
+                        .background(gradientBrush) // 渐变背景
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(8.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+                    ) {
+                        Text("登录", color = Color.White, fontSize = 24.sp)
+                    }
                 }
             }
         }
     }
-}
 }
---
+++ app/src/main/java/com/example/andoirdsecondhandtradingsystem/Start.kt
@@ -0,0 +1,4 @@
+package com.example.andoirdsecondhandtradingsystem
+
+class Start {
+}
